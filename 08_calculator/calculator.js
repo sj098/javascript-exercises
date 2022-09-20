cd873e5a49ae191e0925c9e1595f97cea92b6d27
Suvin Jeon <suvinjeon@gmail.com>
2022-09-20 19:31:27 -0400

    Javascript Exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let total = 0;
+  if (a.length === 0) {
+    return 0
+  }
+  else {
+    for (item of a) {
+      total += item
+    }
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let total = 1;
+  for (item of a) {
+    total = total * item
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let i = 0;
+  let total = 1;
+  while (i < b) {
+    total *= a;
+    i++;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  if (a < 1) {
+    return total;
+  } else {
+    while (a > 0) { 
+      total *= a;
+      a--;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    const newArray = a.split('');
+    function newArr(a) {
+        let i = 0;
+        if (a[i] == " " || a[i] == "," || a[i] == "." || a[i] == "!") {
+            a.splice(i, 0);
+        } else {
+            i++;
+        }
+        return a;
+    }
 
+    newArr(newArray);
+
+    for (let j = 0; j < (newArray.length)/2; j++) {
+        if (newArray[j] === newArray[-j]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 // Do not edit below this line
