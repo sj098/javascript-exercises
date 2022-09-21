e79d510bb74efa0564c03ba1cc939367ab94fb5a
Suvin Jeon <suvinjeon@gmail.com>
2022-09-21 18:42:45 -0400

    JavaScript Exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,25 +1,42 @@
-const palindromes = function (a) {
-    const newArray = a.split('');
-    function newArr(a) {
-        let i = 0;
-        if (a[i] == " " || a[i] == "," || a[i] == "." || a[i] == "!") {
-            a.splice(i, 0);
-        } else {
-            i++;
-        }
-        return a;
-    }
+// const palindromes = function (a) {
+//     const newArray = a.split('');
+//     function newArr(a) {
+//         let i = 0;
+//         if (a[i] == " " || a[i] == "," || a[i] == "." || a[i] == "!") {
+//             a.splice(i, 0);
+//         } else {
+//             i++;
+//         }
+//         return a;
+//     }};
 
-    newArr(newArray);
+//     newArr(newArray);
+//     let j = 0;
+//     while (j < newArray.length/2) {
+//         let result;
+//         for (let k = 0; k < (newArray.length)/2; k++) {
+//             if (newArray[j] === newArray[-j]) {
+//                 result = true;
+//             } else {
+//                 reuslt = false;
+//             }
+//         }
+//         j++;
+//         return result;
+// };
 
-    for (let j = 0; j < (newArray.length)/2; j++) {
-        if (newArray[j] === newArray[-j]) {
-            return true;
-        } else {
-            return false;
-        }
+
+const palindromes = function(a) {
+    let newString = a.toLowerCase().replace(/[^a-z]/g, "");
+    reversedString = newString.split("").reverse().join("");
+    if (reversedString === newString) {
+        return true;
+    } else {
+        return false;
     }
-};
+}
+
+// Learn breakpoints and use to figure out why last test isn't working
 
 // Do not edit below this line
 module.exports = palindromes;
